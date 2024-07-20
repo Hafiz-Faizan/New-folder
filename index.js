7b3257f692b21671809248556dd24db8dd1fcf93
Hafiz-Faizan <111357582+Hafiz-Faizan@users.noreply.github.com>
2024-07-20 14:57:11 +0500

    dsfdf

---
+++ index.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Replace 'YOUR_WEBSITE_URL' with your actual website URL
+    await page.goto('https://www.pmtrainingschool.com/');
+
+    const title = await page.title();
+    console.log('Website Title:', title);
+
+    await browser.close();
+  } catch (err) {
+    console.error(err);
+  }
+})();
